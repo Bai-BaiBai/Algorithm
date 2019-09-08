a8a7c4ac8a98a2125354ecb14bdc2f38d0d72a7e
Bai-BaiBai <877049473@qq.com>
2019-09-08 19:39:47 +0800

    深度优先遍历求联通分量个数

--- Component.h
+++ Component.h
@@ -15,10 +15,12 @@
          Graph &G; // 需要遍历的图
          bool *visited;// 访问标志数组
          int ccount;// 联通分量个数
+         int *id;// 标识每个节点属于的联通分量，可以得出两个节点是否是属于一个联通分量
 
          // 递归完成深度遍历
          void dfs(int v){
              visited[v] = true;
+             id[v] = ccount;
              typename Graph::adjIterator adj(G, v);
              for (int i = adj.begin(); i != adj.end() ; i = adj.next()) {
                  if(!visited[i]){
@@ -30,9 +32,11 @@
      public:
          Component(Graph &graph): G(graph){
              visited = new bool[G.V()];
+             id = new int[G.V()];
              ccount = 0;
              for (int i = 0; i < G.V(); ++i) {
                  visited[i] = false;
+                 id[i] = -1;
              }
 
              for (int i = 0; i < G.V(); ++i) {
@@ -44,6 +48,15 @@
              }
          }
 
+         // 返回两个节点是否相通
+         bool isConnected(int p, int q){
+             if (id[p] == -1 || id[q] == -1) {
+                 return false;
+             } else {
+                 return id[p] == id[q];
+             }
+         }
+
          // 返回联通分量个数
          int count(){
              return ccount;
@@ -51,6 +64,7 @@
 
          ~Component(){
              delete[] visited;
+             delete [] id;
          }
      };
  }
